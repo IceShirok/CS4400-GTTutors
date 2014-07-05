4a35007a499f8ae29d1b4779915f469072e445fd
IceShirok <dongsusanna@gmail.com>
2014-07-05 12:05:14 -0400

    Changed screen size

--- src/code/main/GTTutorsLaunch.java
+++ src/code/main/GTTutorsLaunch.java
@@ -8,8 +8,8 @@ import javafx.stage.Stage;
 
 public class GTTutorsLaunch extends Application {
     
-    public final static int WIDTH = 800;
-    public final static int HEIGHT = 600;
+    public final static int WIDTH = 600;
+    public final static int HEIGHT = 400;
     
     public final static String SCREEN_URL = "../../../res/screens/";
     public final static String FXML_EXT = ".fxml";
