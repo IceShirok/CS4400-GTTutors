408370c8ca25fe139eaa6b3c206b8e809c43f5b0
IceShirok <dongsusanna@gmail.com>
2014-07-05 12:01:55 -0400

    Skeleton for find controlling

--- src/code/controller/FindController.java
+++ src/code/controller/FindController.java
@@ -1,13 +1,35 @@
 package src.code.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import src.code.model.FindPOJO;
 
 public class FindController extends AbstractController {
+    
+    @FXML
+    private TextField gtid;
+    
+    @FXML
+    private TableView<FindPOJO> findTable;
+    
+    @FXML
+    private TableColumn<FindPOJO, String> day;
+    @FXML
+    private TableColumn<FindPOJO, String> time;
+    @FXML
+    private TableColumn<FindPOJO, String> name;
+    @FXML
+    private TableColumn<FindPOJO, String> email;
+    @FXML
+    private TableColumn<FindPOJO, String> course;
 
     @Override
     protected void submit(ActionEvent event) {
-        // TODO Auto-generated method stub
-
+        populateResults();
     }
 
     @Override
@@ -17,8 +39,19 @@ public class FindController extends AbstractController {
 
     @Override
     protected void populate(ActionEvent event) {
-        // TODO Auto-generated method stub
+        // do not implement - not needed
+    }
+    
+    private void populateResults() {
+        System.out.println(gtid.getText());
+        // TODO: implement
+        day.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("day"));
+        time.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("time"));
+        name.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("name"));
+        email.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("email"));
+        course.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("course"));
         
+        // put database manipulation here
     }
 
 }
---
+++ src/code/model/FindPOJO.java
@@ -0,0 +1,39 @@
+package src.code.model;
+
+public class FindPOJO {
+    
+    private String day;
+    private String time;
+    private String name;
+    private String email;
+    private String course;
+    
+    public FindPOJO(String day, String time, String name, String email,
+            String course) {
+        this.day = day;
+        this.time = time;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
