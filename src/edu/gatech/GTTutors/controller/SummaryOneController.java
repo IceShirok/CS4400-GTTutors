d9ab2206c824a5fa7661c0d6793319fb61cfc2a4
Tyler Pennington <tylerpnn@gmail.com>
2014-07-21 17:48:54 -0400

    summary 1

--- src/edu/gatech/GTTutors/controller/SummaryOneController.java
+++ src/edu/gatech/GTTutors/controller/SummaryOneController.java
@@ -1,8 +1,15 @@
 package edu.gatech.GTTutors.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -10,6 +17,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import edu.gatech.GTTutors.main.DatabaseController;
 import edu.gatech.GTTutors.model.Sum1POJO;
 
 public class SummaryOneController extends AbstractController {
@@ -40,13 +48,13 @@ public class SummaryOneController extends AbstractController {
     protected void submit(ActionEvent event) {
         Set<String> semestersSet = new LinkedHashSet<>();
         if(fall.isSelected()) {
-            semestersSet.add("Fall");
+            semestersSet.add("\"Fall\"");
         }
         if(spring.isSelected()) {
-            semestersSet.add("Spring");
+            semestersSet.add("\"Spring\"");
         }
         if(summer.isSelected()) {
-            semestersSet.add("Summer");
+            semestersSet.add("\"Summer\"");
         }
         
         if(semestersSet.isEmpty()) {
@@ -61,10 +69,10 @@ public class SummaryOneController extends AbstractController {
     protected void goBack(ActionEvent event) {
         transition(event, "menu");
     }
-
+    
     @Override
     protected void populate(ActionEvent event) {
-        // do not implement - not needed
+        
     }
     
     private void populateResults(String semesters) {
@@ -74,7 +82,39 @@ public class SummaryOneController extends AbstractController {
         numStudents.setCellValueFactory(new PropertyValueFactory<Sum1POJO,Integer>("numStudents"));
         numTutors.setCellValueFactory(new PropertyValueFactory<Sum1POJO,Integer>("numTutors"));
         
-        // put database manipulation here
+        String query = "select H.School, H.Number, H.Semester, count(DISTINCT H.GTID) as SGTID, count(DISTINCT S.GTID) as TGTID" +
+ 			   " from Hires H left outer join TimeSlot S on S.Time=H.Time and S.Weekday=H.Weekday and S.Semester=H.Semester" +
+ 			   " where H.Semester IN " + semesters +
+ 			   " group by H.School, H.Number, H.Semester" +
+ 			   " order by H.School ASC";
+        System.out.println(query);
+        
+        Connection connect = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = (Connection) DriverManager.getConnection(DatabaseController.DB_URL + DatabaseController.GROUP,
+                                                                DatabaseController.GROUP,
+                                                                DatabaseController.PW);
+            Statement stmt = connect.createStatement();
+            ResultSet rset = stmt.executeQuery(query);
+            
+            ObservableList<Sum1POJO> rows = FXCollections.observableArrayList();
+            while(rset.next()) {
+            	String course = rset.getString("School") + rset.getString("Number");
+            	String semester = rset.getString("Semester");
+            	int numStudents = rset.getInt("SGTID");
+            	int numTutors = rset.getInt("TGTID");
+            	rows.add(new Sum1POJO(course, semester, numStudents, numTutors));            	
+            }
+            sum1Table.setItems(rows);
+        } catch(Exception e) {
+        	e.printStackTrace();
+        } finally {
+        	try {
+	        	if(connect != null)
+	        		connect.close();
+        	} catch(SQLException e) {}
+        }
     }
 
 }
