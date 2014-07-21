688ec6efbd3808dc1ff232e0e205a9520356cc32
IceShirok <dongsusanna@gmail.com>
2014-07-21 10:45:37 -0400

    Basics for find controller

--- res/db/cs4400-sql-statements.sql
+++ res/db/cs4400-sql-statements.sql
@@ -4,7 +4,17 @@
 =======================
 */
 
-INSERT INTO Recommends VALUES ("900000003", "700000000", "This student was the best student I've ever had.", 1);
+/*
+CREATE VIEW HiringTutorInfo AS (
+	SELECT T.GTID AS TGTID,
+	H.GTID AS UGTID, S.Email, S.Name,
+	T.School, T.Number,
+	H.Time, H.Semester, H.Weekday
+	FROM (Tutors T INNER JOIN Hires H ON T.School=H.School AND T.Number=H.Number)
+	INNER JOIN TimeSlot TS ON TS.Time=H.Time AND TS.Semester=H.Semester AND TS.Weekday=H.Weekday AND TS.GTID=T.GTID
+	INNER JOIN Student S ON H.GTID=S.GTID
+);
+*/
 
 /* RECOMMENDS SCREEN */
 INSERT INTO Recommends VALUES ("900000003", "700000000", "This student was the best student I've ever had.", 1);
--- src/edu/gatech/GTTutors/controller/FindController.java
+++ src/edu/gatech/GTTutors/controller/FindController.java
@@ -1,14 +1,27 @@
 package edu.gatech.GTTutors.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import edu.gatech.GTTutors.main.DatabaseController;
+import edu.gatech.GTTutors.main.GTTutorsLaunch;
 import edu.gatech.GTTutors.model.FindPOJO;
 
 public class FindController extends AbstractController {
+    
+    @FXML
+    private Label message;
     
     @FXML
     private TextField gtid;
@@ -29,7 +42,12 @@ public class FindController extends AbstractController {
 
     @Override
     protected void submit(ActionEvent event) {
-        populateResults();
+        if(!GTTutorsLaunch.log.getUsername().equals(gtid.getText())) {
+            message.setText("You can only see your own schedule.");
+            return;
+        } else {
+            populateResults();
+        }
     }
 
     @Override
@@ -43,7 +61,7 @@ public class FindController extends AbstractController {
     }
     
     private void populateResults() {
-        System.out.println(gtid.getText());
+        
         // TODO: implement
         day.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("day"));
         time.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("time"));
@@ -51,7 +69,43 @@ public class FindController extends AbstractController {
         email.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("email"));
         course.setCellValueFactory(new PropertyValueFactory<FindPOJO,String>("course"));
         
-        // put database manipulation here
-    }
+        Connection connect = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = (Connection) DriverManager.getConnection(DatabaseController.DB_URL + DatabaseController.GROUP,
+                                                                DatabaseController.GROUP,
+                                                                DatabaseController.PW);
+            Statement stmt = connect.createStatement();
 
+            String strSelect = "SELECT Weekday, Time, Name, Email, School, Number"
+                    + " FROM HiringTutorInfo WHERE TGTID=\"" + GTTutorsLaunch.log.getUsername() + "\""
+                    + " AND Semester=\"" + GTTutorsLaunch.log.getCurrentSemester() + "\";";
+            System.out.println(strSelect);
+            
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // TODO: store the db info into the table
+            
+            ObservableList<FindPOJO> tableData = FXCollections.observableArrayList();
+            while(rset.next()) {
+                String day = rset.getString("Weekday");
+                String time = rset.getString("Time");
+                String name = rset.getString("Name");
+                String email = rset.getString("Email");
+                String course = rset.getString("School") + " " + rset.getString("Number");
+                System.out.println(day);
+                tableData.add(new FindPOJO(day, time, name, email, course));
+            }
+            findTable.setItems(tableData);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            message.setText("uh-oh, something went wrong.");
+        } finally {
+            try {
+                connect.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
